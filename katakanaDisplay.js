2ddbbbe07fa72ab920addb74a7907f4e790a3e7e
judetsuki <Luka9609@gmail.com>
2025-06-13 16:13:22 +0300

    simplified imports and file structure

---
+++ katakanaDisplay.js
@@ -0,0 +1,44 @@
+import katakanaList from './assets/katakanaList.js';
+
+const SymbolBox = document.querySelector(".symbol-box");
+const answerBox = document.querySelector(".answer-box");
+const scoreDisplay = document.querySelector(".score-display");
+
+let symbols = Object.keys(katakanaList);
+let currentSymbol = null;
+let currentIndex = 0;
+let correctGuesses = 0;
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function showNextSymbol() {
+  if (currentIndex >= symbols.length) {
+    shuffle(symbols);
+    currentIndex = 0;
+  }
+  const randomKey = symbols[currentIndex];
+  currentSymbol = katakanaList[randomKey];
+  SymbolBox.textContent = currentSymbol;
+  answerBox.value = "";
+  currentIndex+=1;
+}
+
+shuffle(symbols);
+showNextSymbol();
+
+answerBox.addEventListener("input", () => {
+  const userAnswer = answerBox.value;
+  const correctAnswer = Object.keys(katakanaList).find(
+    (key) => katakanaList[key] === currentSymbol
+  );
+  if (userAnswer === correctAnswer) {
+    showNextSymbol();
+    correctGuesses +=1;
+    scoreDisplay.textContent = `Correct Guesses: ${correctGuesses}`;
+  }
+});
